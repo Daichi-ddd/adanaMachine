9da97257e3a8e8d1f31c19327c7f6ddcf189c0a7
Daichi Nakama <daichinakama.tech@gmail.com>
2021-06-29 12:39:06 +0900

    first commit

---
+++ adana.js
@@ -0,0 +1,29 @@
+let namae = ["わく", "ちん", "ぽん"];
+let adana;
+let keisyou = "くん"
+var keisyouCount = Math.floor(Math.random() * (3 + 1 - 1));
+console.log(keisyouCount);
+console.log(namae[keisyouCount]);
+
+let adanaMachine = (namae, keisyouCount) => {
+
+    let karina = namae[keisyouCount] + namae[keisyouCount];
+
+    switch (keisyouCount) {
+        case 0:
+            adana = karina + "くん"
+            break;
+        case 1:
+            adana = karina + "ちゃん"
+            break;
+        case 2:
+            adana = karina + "さん"
+            break;
+
+
+    }
+
+    return adana;
+};
+
+document.getElementById('result').textContent = adanaMachine(namae, keisyouCount);
